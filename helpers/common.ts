dd929281ad1c7715885ac169eef75d26404f2fec
pratham-rabari <153089464+pratham-rabari@users.noreply.github.com>
2025-02-07 14:08:45 +0530

    Update common.ts
    render 16

--- helpers/common.ts
+++ helpers/common.ts
@@ -60,23 +60,38 @@ export default class CommonTaskController {
   }
 
   // To generate JWT token
-  generateJWT(tokenObj: any, expiry?: string) {
-    if(!tokenObj){
-      return;
-    }
-    let obj = { ...tokenObj }
-    let token: any;
-    if (expiry) {
-      token = jwt.sign(
-        { ...tokenObj },
-        process.env.JWTSECRET,
-        { expiresIn: expiry }
-      );
-    } else {
-      token = jwt.sign({ ...tokenObj }, process.env.JWTSECRET);
-    }
-    return token;
+  // generateJWT(tokenObj: any, expiry?: string) {
+  //   if(!tokenObj){
+  //     return;
+  //   }
+  //   let obj = { ...tokenObj }
+  //   let token: any;
+  //   if (expiry) {
+  //     token = jwt.sign(
+  //       { ...tokenObj },
+  //       process.env.JWTSECRET,
+  //       { expiresIn: expiry }
+  //     );
+  //   } else {
+  //     token = jwt.sign({ ...tokenObj }, process.env.JWTSECRET);
+  //   }
+  //   return token;
+  // }
+
+   generateJWT(tokenObj: any, expiry?: string): string {
+  const secretKey = process.env.JWTSECRET as string;
+
+  // Ensure secretKey is defined
+  if (!secretKey) {
+    throw new Error("JWTSECRET is not defined in environment variables");
   }
+
+  const token = expiry
+    ? jwt.sign({ ...tokenObj }, secretKey, { expiresIn: expiry })
+    : jwt.sign({ ...tokenObj }, secretKey);
+
+  return token;
+}
 
   async AddActivityLog(data) {
     try {
